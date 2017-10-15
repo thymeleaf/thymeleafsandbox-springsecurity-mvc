cb606e34b0cac8f5ee13f00f7f3c10a488b98d83
Daniel Fernández <daniel.fernandez@11thlabs.org>
2017-10-15 18:13:39 +0200

    Added first version of project, adapted from the example app

---
+++ src/main/java/thymeleafsandbox/springsecurity/SpringSecurityMvcApplication.java
@@ -0,0 +1,12 @@
+package thymeleafsandbox.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMvcApplication.class, args);
+	}
+}
