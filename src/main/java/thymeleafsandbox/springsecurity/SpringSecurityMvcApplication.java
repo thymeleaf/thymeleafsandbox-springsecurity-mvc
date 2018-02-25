f7ef5c5afa6438016ae483a0c64c5456e65a66bf
Daniel Fernández <daniel.fernandez@11thlabs.org>
2018-02-26 00:39:29 +0100

    Added declaration of SpringSecurityDialect

--- src/main/java/thymeleafsandbox/springsecurity/SpringSecurityMvcApplication.java
+++ src/main/java/thymeleafsandbox/springsecurity/SpringSecurityMvcApplication.java
@@ -2,9 +2,22 @@ package thymeleafsandbox.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class SpringSecurityMvcApplication {
+
+
+
+
+	// TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+	// TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
+	@Bean
+	public SpringSecurityDialect securityDialect() {
+		return new SpringSecurityDialect();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityMvcApplication.class, args);
